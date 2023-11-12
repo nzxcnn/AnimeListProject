b39473bd93f80fe568ed849aec13c8861daab3cc
Алексей Ли <Alexey>
2023-11-12 18:22:52 +0600

    First commit

---
+++ site/src/main/java/com/AnimeList/demo/AnimeListApplication.java
@@ -0,0 +1,13 @@
+package com.AnimeList.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeListApplication.class, args);
+	}
+
+}
